246fa52b549ef1188b65f5b150c1c6da6a2277f0
Bright Otu <bionethe69@gmail.com>
2023-07-02 15:19:48 -0700

    0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Pascal triangle implementation
+"""
+
+
+def pascal_triangle(n:int):
+    """
+    Pascal Triangle
+    """
+    if n<=0:
+        return[]
+    if n==2:
+        return [[1], [1,1]]
+
+    triangle = [[1], [1, 1]]
+
+    for _ in range(2, n):
+        temp =[1, 1]
+        for j in range(0, len(triangle[-1])-1):
+            a = triangle[-1][j]
+            b = triangle[-1][j+1]
+            temp.insert(-1, a+b)
+            triangle.append(temp)
+
+    return triangle
